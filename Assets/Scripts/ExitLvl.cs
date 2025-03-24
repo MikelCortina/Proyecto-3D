89b54db8b053c6e7dee9dca543f198002925688d
MikelCortina <mikel_cortina@euneiz.com>
2025-03-24 01:15:18 +0100

    Nota Mental cambiar de ordenador, por que vaya puta cafetera tengo
    La mitad del dia añadiendo las mismas cosas al inspector sin parar por que no entiendo como ostias van los prefabs, y la otra mitad en el photoshop pa cosas que seguramente tiure que rabia, al menos el skybox que he hecho si me lo curro suficiente puede quedar muy guay, veremos

--- Assets/Scripts/ExitLvl.cs
+++ Assets/Scripts/ExitLvl.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,9 @@ public class LevelManager : MonoBehaviour
     public int killCountObjc;
     public GameObject startPanel; // Panel que se activará al cargar la escena
     public bool playable=false;
+    public BestTimesManager bestTimesManager;
+    public EndLevelUI endLevel;
+    public TextMeshProUGUI bestTimesText;
 
 
     private void Start()
@@ -17,6 +21,14 @@ public class LevelManager : MonoBehaviour
         {
             startPanel.SetActive(true);
             Time.timeScale = 0f;
+
+            string bestTimesDisplay = "";
+            float[] bestTimes = bestTimesManager.GetBestTimes(endLevel.level);
+            for (int i = 0; i < bestTimes.Length; i++)
+            {
+                bestTimesDisplay += $"{i + 1}: {bestTimes[i]:F2} s\n";
+            }
+            bestTimesText.text = bestTimesDisplay;
         }
     }
 
@@ -28,6 +40,10 @@ public class LevelManager : MonoBehaviour
             startPanel.SetActive(false);
             playable = true;
             Time.timeScale = 1f;
+        }
+        if (!playable && Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MenuPrincipal"); // Cambia "GameScene" por el nombre de tu escena de juego
         }
 
         if (Input.GetKeyDown(KeyCode.F))
